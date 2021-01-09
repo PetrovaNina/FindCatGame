7f530b0e0a44859d2ce824885e8ea2b1cdf651eb
PetrovaNina <petrova1nina@gmail.com>
2021-01-10 01:52:06 +0300

    Turn code into object

--- findCat.js
+++ findCat.js
@@ -1,44 +1,43 @@
-const NUM_CATS = 3;
-const CAT_LOCATIONS = [["06", "16", "26"], ["24", "34", "44"], ["10", "11", "12"]];
-const IMGS = [
-    "img/1.png",
-    "img/2.png",
-    "img/3.png",
-    "img/4.png",
-    "img/5.png",
-    "img/6.png",
-    "img/7.png",
-    "img/8.png",
-    "img/9.png",
-    "img/10.png"
-];
-
-function click(tag, guess) {
-    for (let i = 0; i < NUM_CATS; i++) {
-        const index = CAT_LOCATIONS[i].indexOf(guess);
-        if (index >= 0) {
-            return tag.setAttribute("class", "hit");
+const MODEL = {
+    numCats: 3,
+    catLocations: [["06", "16", "26"], ["24", "34", "44"], ["10", "11", "12"]],
+    catImages: [
+        "img/1.png",
+        "img/2.png",
+        "img/3.png",
+        "img/4.png",
+        "img/5.png",
+        "img/6.png",
+        "img/7.png",
+        "img/8.png",
+        "img/9.png",
+        "img/10.png"
+    ],
+    clickOnCell: function (tag, cellId) {
+        for (let i = 0; i < this.numCats; i++) {
+            const index = this.catLocations[i].indexOf(cellId);
+            if (index >= 0) {
+                return tag.setAttribute("class", "hit");
+            }
+        }
+        return tag.setAttribute("class", "miss");
+    },
+    changeImageOnClick: function () {
+        const self = this;
+        const tds = document.querySelectorAll("td");
+        for (const td of tds) {
+            td.onclick = function () {
+                self.clickOnCell(td, td.id);
+                if (td.classList.contains("hit")) {
+                    const catIndex = Math.floor(Math.random() * self.catImages.length - 1);
+                    td.style.backgroundImage = 'url(' + self.catImages[catIndex] + ')';
+                    self.catImages.splice(catIndex, 1);
+                } else {
+                    td.style.backgroundColor = '#8e381e';
+                }
+            }
         }
     }
-    return tag.setAttribute("class", "miss");
-}
+};
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-
-const TDS = document.querySelectorAll("td");
-
-for (const td of TDS) {
-    td.onclick = function () {
-        console.log(click(td, td.id));
-        if (td.classList.contains("hit")) {
-            let catIndex = getRandomInt(IMGS.length - 1);
-            td.style.backgroundImage = 'url(' + IMGS[catIndex] + ')';
-            IMGS.splice(catIndex, 1);
-        } else {
-            td.style.backgroundColor = '#8e381e';
-        }
-    }
-}
+MODEL.changeImageOnClick();
