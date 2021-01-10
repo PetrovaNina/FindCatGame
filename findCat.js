ee84dceb9b7ade28e4ca55ab8f00b38d7762b846
PetrovaNina <petrova1nina@gmail.com>
2021-01-10 16:19:26 +0300

    Put cell style changing in clickOnCell method

--- findCat.js
+++ findCat.js
@@ -2,15 +2,21 @@ const MODEL = {
     numCats: 3,
     catLocations: [["06", "16", "26"], ["24", "34", "44"], ["10", "11", "12"]],
     excludedCatImages: [],
-    clickOnCell: function (tag, cellId) {
+    clickOnCell: function (tag) {
+        if (tag.classList.contains("hit") || tag.classList.contains("miss")) {
+            return;
+        }
         for (let i = 0; i < this.numCats; i++) {
-            const index = this.catLocations[i].indexOf(cellId);
+            const index = this.catLocations[i].indexOf(tag.id);
             if (index >= 0) {
-                return tag.setAttribute("class", "hit");
+                tag.style.backgroundImage = this.getRandomImage();
+                tag.setAttribute("class", "hit");
+                return;
             }
         }
-        return tag.setAttribute("class", "miss");
+        tag.setAttribute("class", "miss");
     },
+
     getRandomImage: function () {
         // choosing random name of images from 1.png to 10.png
         const randNum = Math.floor(Math.random() * 9 + 1);
@@ -20,17 +26,14 @@ const MODEL = {
         }
         return null;
     },
+
     changeImageOnClick: function () {
         const self = this;
         const tds = document.querySelectorAll("td");
         for (const td of tds) {
             td.onclick = function () {
-                self.clickOnCell(td, td.id);
-                if (td.classList.contains("hit")) {
-                    td.style.backgroundImage = self.getRandomImage();
-                } else {
-                    td.style.backgroundColor = '#8e381e';
-                }
+                self.clickOnCell(td);
+                console.log(self.excludedCatImages)
             }
         }
     }
